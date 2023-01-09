88e015d8afdf7dcd7837115fe2ec1adfc588ca8c
Megh2507 <megha25072001@gmail.com>
2023-01-09 12:06:12 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from sklearn.ensemble import RandomForestClassifier
+import  pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import  accuracy_score
+from PIL import Image
+# import streamlit as st
+print("hi")
+
+
+
